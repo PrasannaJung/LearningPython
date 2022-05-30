9a9a3107e9cc2ca9116b6cd560bdaa7f6abaf8f7
PrasannaJung <prasannajung08@gmail.com>
2022-05-31 00:01:12 +0545

    Circles pattern

---
+++ Turtle/mult_circles.py
@@ -0,0 +1,30 @@
+import turtle
+
+t = turtle.Turtle()
+t.up()
+t.goto(0, -100)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(350,200)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(350,-350)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(-350,200)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(-350,-350)
+t.down()
+t.circle(100)
+
+t.ht()
+turtle.mainloop()
