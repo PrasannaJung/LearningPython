67c2ff76c733ebc15bddb78f6eeca5ba0b785c18
PrasannaJung <prasannajung08@gmail.com>
2022-05-31 09:22:01 +0545

    update star pattern

--- Turtle/star.py
+++ Turtle/star.py
@@ -1,7 +1,14 @@
 import turtle
 window = turtle.Turtle()
+turtle.bgcolor("purple")
+window.speed(3)
+window.pensize(6)
+window.color("white")
+window.up()
+window.goto(-200,0)
+window.down()
 for i in range(5):
-		window.forward(100)
+		window.forward(400)
 		window.right(144)
   
   
