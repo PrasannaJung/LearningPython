522adfcd20ba365fd17840f7e73eaedf879490f8
PrasannaJung <prasannajung08@gmail.com>
2022-06-11 20:13:40 +0545

    Final Commit

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title('Simple calculator')
+root.minsize(width=300, height=400)
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
@@ -45,8 +46,17 @@ def button_mult():
     global math
     math = 'mult'
     f_num = int(first_number)
-    e.delete(0, END)        
+    e.delete(0, END)     
 
+def button_modulo():
+    first_number = e.get()
+    global f_num
+    global math
+    math = 'modulo'
+    f_num = int(first_number)
+    e.delete(0, END)
+
+        
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
@@ -58,6 +68,8 @@ def button_equal():
         e.insert(0, f_num * int(second_number))
     elif math == 'div':
         e.insert(0, f_num / int(second_number))
+    elif math == 'modulo':
+        e.insert(0, f_num % int(second_number))    
 
 
 
@@ -72,34 +84,36 @@ button_7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text='+', padx=39, pady=20, command=button_add)
-button_sub = Button(root, text='-', padx=41, pady=20, command=button_sub)
-button_div = Button(root, text='/', padx=41, pady=20, command=button_div)
-button_mult = Button(root, text='*', padx=40, pady=20, command=button_mult)
-button_equal = Button(root, text='=', padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text='Clear', padx=79, pady=20, command=button_clear)
+button_add = Button(root,bg="purple",fg="white", text='+', padx=39, pady=20, command=button_add)
+button_sub = Button(root,bg="purple",fg="white", text='-', padx=41, pady=20, command=button_sub)
+button_mult = Button(root,bg="purple",fg="white", text='*', padx=40, pady=20, command=button_mult)
+button_div = Button(root,bg="purple",fg="white",text='/', padx=41, pady=20, command=button_div)
+button_modulo = Button(root,bg="purple",fg="white", text='%', padx=40, pady=20, command=button_modulo)
+button_equal = Button(root,bg="yellow", text='=', padx=41, pady=20, command=button_equal)
+button_clear = Button(root,bg='#444',fg="white", text='C', padx=41, pady=20, command=button_clear)
 
 
 
 # putting the buttons on the screen
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+button_clear.grid(row=1, column=0,columnspan=1)
+button_modulo.grid(row=1, column=1)
+button_div.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
-button_sub.grid(row=6, column=0)
-button_div.grid(row=6, column=1)
-button_mult.grid(row=6, column=2)
+button_7.grid(row=2, column=0)
+button_add.grid(row=2, column=3)
+button_equal.grid(row=4, column=3)
+button_sub.grid(row=3, column=3)
+button_9.grid(row=2, column=2)
+button_mult.grid(row=1, column=3)
+button_8.grid(row=2, column=1)
 
 root.mainloop()
